23757ee9dc23eef1f0412c2e7932fe255f641a84
NSTCarloMtz <al02589690@tecmilenio.mx>
2018-06-19 12:05:25 -0500

    feat(*): Completed module no. 2 from Angular Fundamentals course.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component'
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    NavBarComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
