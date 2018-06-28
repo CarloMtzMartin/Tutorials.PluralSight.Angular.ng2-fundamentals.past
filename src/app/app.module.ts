26e1d8bd82359f0f2425315ae0eb790520ca0b30
NSTCarloMtz <al02589690@tecmilenio.mx>
2018-06-28 13:12:35 -0500

    feat(*): Completed module no. 4 from Angular Fundamentals course.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component'
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     NavBarComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
