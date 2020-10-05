f4b696862515b9291808c8dada8912fd90bdb6a5
Carlo Martinez <carlo.cool13@gmail.com>
2020-10-05 14:35:02 -0500

    feat(*): Update profile component.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
 
 @Component({
   template: `
     <h1>Edit Your Profile</h1>
-    <hr>
+    <hr />
     <div class="col-md-6">
       <h3>[Edit profile form will go here]</h3>
+      <img
+        src="https://www.pikpng.com/pngl/m/80-805523_default-avatar-svg-png-icon-free-download-264157.png"
+      />
       <br />
       <br />
       <button type="submit" class="btn btn-primary">Save</button>
@@ -13,6 +16,4 @@ import { Component } from '@angular/core'
     </div>
   `,
 })
-export class ProfileComponent {
-       
-}
+export class ProfileComponent {}
